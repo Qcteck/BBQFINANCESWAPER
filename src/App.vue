0791f53cbfcf860040d0d250a0438f5d22b65bfc
Qcteck <178105753+Qcteck@users.noreply.github.com>
2024-12-16 20:15:44 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+const config = {
+  variant: "wide",
+  appearance: "auto",
+  theme: {
+    palette: {
+      primary: {
+        main: "#ec0000"
+      },
+      secondary: {
+        main: "#ec0000"
+      },
+      text: {
+        secondary: "#f7ce46"
+      }
+    },
+    typography: {
+      fontFamily: "Inter, sans-serif"
+    },
+    container: {
+      boxShadow: "0px 8px 32px rgba(0, 0, 0, 0.08)",
+      borderRadius: "16px"
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
